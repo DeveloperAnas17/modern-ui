4f35f81acab9e29a36944fa4a4fd702fa6273d8b
Anas <amanabbasi3660@gmail.com>
2021-11-15 21:13:05 +0530

    add footer

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -1,12 +1,51 @@
 import React from "react";
+import gpt3logo from "../../assets/logo.svg";
 import "./Footer.css";
 
-const Footer = () => {
-  return (
-    <div>
-      <h3>Footer</h3>
+const Footer = () => (
+  <div className="footer section__padding">
+    <div className="footer-heading">
+      <h1 className="gradient__text">
+        Do you want to step in to the future before others
+      </h1>
     </div>
-  );
-};
+
+    <div className="footer__btn">
+      <p>Request Early Access</p>
+    </div>
+
+    <div className="footer-links">
+      <div className="footer-links_logo">
+        <img src={gpt3logo} alt="ogo" />
+        <p>
+          Crechterwoord K12 182 DK Alknjkcb, <br /> All Rights Reserved
+        </p>
+      </div>
+      <div className="footer-links_div">
+        <h4>Links</h4>
+        <p>Overons</p>
+        <p>Social Media</p>
+        <p>Counters</p>
+        <p>Contact</p>
+      </div>
+      <div className="footer-links_div">
+        <h4>Company</h4>
+        <p>Terms & Conditions </p>
+        <p>Privacy Policy</p>
+        <p>Contact</p>
+      </div>
+      <div className="footer-links_div">
+        <h4>Get in touch</h4>
+        <p>Crechterwoord K12 182 DK Alknjkcb</p>
+        <p>085-132567</p>
+        <p>info@payme.net</p>
+      </div>
+    </div>
+
+    <div className="footer-copyright">
+      <p>@2021 GPT-3. All rights reserved.</p>
+    </div>
+  </div>
+);
 
 export default Footer;
