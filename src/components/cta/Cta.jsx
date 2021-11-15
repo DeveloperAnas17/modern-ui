7011209957e7adbcab67e3dccf76aa670499b891
Anas <amanabbasi3660@gmail.com>
2021-11-15 21:06:00 +0530

    add cta

--- src/components/cta/Cta.jsx
+++ src/components/cta/Cta.jsx
@@ -3,8 +3,14 @@ import "./Cta.css";
 
 const Cta = () => {
   return (
-    <div>
-      <h3>CTA</h3>
+    <div className="cta">
+      <div className="cta__content">
+        <p>Request Early Access to Get Started</p>
+        <h3>Register Today & start exploring the endless possibilities.</h3>
+      </div>
+      <div className="cta__button">
+        <button type="button">Get Started</button>
+      </div>
     </div>
   );
 };
